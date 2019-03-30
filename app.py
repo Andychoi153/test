2c83980bf1c03df360fc86fd7e59c0b6c8ee6fef
Dae Bok Choi <andychoi153@gmail.com>
2019-03-30 18:27:55 +0900

    port 80

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, world!'
 
+
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='127.0.0.1', port=80)
